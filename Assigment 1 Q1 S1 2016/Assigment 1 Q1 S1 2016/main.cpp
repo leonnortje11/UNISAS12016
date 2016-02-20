c9548375fb7177ecd5d27b04a3a38694ef118809
leonnortje11 <nortje.leon@gmail.com>
2016-02-21 00:40:12 +0200

    Final

--- Assigment 1 Q1 S1 2016/Assigment 1 Q1 S1 2016/main.cpp
+++ Assigment 1 Q1 S1 2016/Assigment 1 Q1 S1 2016/main.cpp
@@ -4,28 +4,34 @@
 //
 //  Created by Leon Nortje on 2016/02/20.
 //  Copyright © 2016 Leon Nortje. All rights reserved.
-//
+//  Assignment 1 Question 1
+//  calculate total discount
 
 #include <iostream>
 using namespace std;
 
 double calculated_dicount(double total_bill_amount, double discount_percentage);
+//Calculate total discount for none Thursday.
 
 double calculated_discount(double total_bill_amount, double discount_percentage, int thursday_discount);
+//Calculate total discount if user selects Thursday and nominated discount.
 
 int main()
 {
-    double total_bill_amount;
-    int discount_percentage = 0;
-    char status;
+    double total_bill_amount, total_discount;
+    int discount_percentage = 0, thursday_discount;
+    char status, answer;
     
     cout << "Please enter bill amount: ";
     cin >> total_bill_amount;
     
-    cout << "Please enter your membership status: ";
-    cin >> status;
-
+    cout << "Please enter your membership status:\n"
+        << "S = Silver 5%\n"
+        << "G = Gold 10%\n"
+        << "P = Platinum 15%\n"
+        << "Selection: ";
     
+    cin >> status;
     
     switch (status)
     {
@@ -47,13 +53,57 @@ int main()
         default:
             cout << "Selection not posible:";
     
-            cout << discount_percentage;
             
     }
+    
+  //  cout << discount_percentage;
 
+    cout << "Is it Thursday?: ";
+    cin >> answer;
     
-    
+    if (answer == 'Y')
+    {
+        cout << "Please choose discount percentage 5% or 10%: ";
+        cin >> thursday_discount;
+        cout << "You have selected " << thursday_discount << "%\n";
+        
+        total_discount = calculated_discount(total_bill_amount, discount_percentage, thursday_discount);
+        cout << total_discount;
+        cout << endl;
+    }
+
+else
+{
+    total_discount = calculated_dicount(total_bill_amount, discount_percentage);
+    cout << total_discount;
     cout << endl;
+}
+    
     
     return 0;
 }
+
+double calculated_dicount(double total_bill_amount, double discount_percentage)
+//Non Thursday selection.
+{
+    
+    double total;
+    
+    total = total_bill_amount - (discount_percentage/100)*total_bill_amount;
+    return (total);
+    
+}
+
+double calculated_discount(double total_bill_amount, double discount_percentage, int thursday_discount)
+//Discount calculation based on a Thursday selection.
+{
+    double total2, total_percentage_discount;
+    
+    total_percentage_discount = discount_percentage + thursday_discount;
+
+    total2 = total_bill_amount - (total_percentage_discount/100)*total_bill_amount;
+    
+    return (total2);
+    
+}
+
