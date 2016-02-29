b4c61de9df3e3fa4e900daf16e95702822519c15
leonnortje11 <nortje.leon@gmail.com>
2016-02-29 21:18:40 +0200

    Final

--- Assignment 1 Q2 S1 2016/Assignment 1 Q2 S1 2016/main.cpp
+++ Assignment 1 Q2 S1 2016/Assignment 1 Q2 S1 2016/main.cpp
@@ -1,48 +1,95 @@
-
-//  Created by Leon Nortje on 2016/02/27.
-//  Copyright © 2016 Leon Nortje. All rights reserved.
-//
+//  File Name: main.cpp
+//  Created by Leon Nortje
+//  Email Address: nortje.leon@gmail.com
+//  Copyright (c) 2014 Leon Nortje. All rights reserved.
+//  Description: Calculate APS score for BSc admission
 
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 int main()
 {
-    int i, score[6], max[6], total;
+    int i, score[3], max[3], score2[3], max2[3], total;
     
-    cout << "Enter 5 scores:\n";
+    cout << "Enter first 3 scores of Englisg, Matchematics & Physical Science:\n";
     cin >> score[0];
     max[0] = 0;
     
-    for (i = 1; i < 6; i++)
+    if (score[0] >= 80 && score[0] <= 100)
+        max[0] = 6;
+    if (score[0] >= 70 && score[0] <= 79)
+        max[0] = 5;
+    if (score[0] >= 60 && score[0] <= 69)
+        max[0] = 4;
+    if (score[0] >= 50 && score[0] <= 59)
+        max[0] = 3;
+    if (score[0] < 50)
+        max[0] = 0;
+    assert(max[0] >= 4);
+    
+    for (i = 1; i < 3; i++)
     {
         cin >> score[i];
         
-        if (score[i] > 80 && score[i] < 100)
-            max[i] = 4;
-        if (score[i] > 70 && score[i] < 79)
+        if (score[i] >= 80 && score[i] <= 100)
+            max[i] = 6;
+        if (score[i] >= 70 && score[i] <= 79)
             max[i] = 5;
-        if (score[i] > 60 && score[i] < 69)
+        if (score[i] >= 60 && score[i] <= 69)
             max[i] = 4;
-        if (score[i] > 50 && score[i] < 59)
+        if (score[i] >= 50 && score[i] <= 59)
             max[i] = 3;
         if (score[i] < 50)
             max[i] = 0;
+        assert(max[i] >= 4);
     }
     
-    cout << max[0] << endl;
-    cout << max[1] << endl;
-    cout << max[2] << endl;
-    cout << max[3] << endl;
-    cout << max[4] << endl;
-    cout << max[5] << endl;
+    cout << "You have the required scores for English, Mathematics & Physical Science.\n"
+        << "Please enter the scores for the last three subjects:\n";
+
+    cin >> score2[0];
+    max2[0] = 0;
     
-    total = max[0] + max[1] + max[2] + max[3] + max[4] + max[5];
+    if (score2[0] >= 80 && score2[0] <= 100)
+        max2[0] = 6;
+    if (score2[0] >= 70 && score2[0] <= 79)
+        max2[0] = 5;
+    if (score2[0] >= 60 && score2[0] <= 69)
+        max2[0] = 4;
+    if (score2[0] >= 50 && score2[0] <= 59)
+        max2[0] = 3;
+    if (score2[0] < 50)
+        max2[0] = 0;
+    assert(score2[i] >= 0 && score2[1] <=100);
+    
+    for (i = 1; i < 3; i++)
+    {
+        cin >> score2[i];
+        
+        if (score2[i] >= 80 && score2[i] <= 100)
+            max2[i] = 6;
+        if (score2[i] >= 70 && score2[i] <= 79)
+            max2[i] = 5;
+        if (score2[i] >= 60 && score2[i] <= 69)
+            max2[i] = 4;
+        if (score2[i] >= 50 && score2[i] <= 59)
+            max2[i] = 3;
+        if (score2[i] < 50)
+            max2[i] = 0;
+        assert(score2[i] >= 0 && score2[1] <=100);
+    }
+
+    
+    total = max[0] + max[1] + max[2] + max2[0] + max2[1] + max2[2];
     
     
+    cout << "Your APS score is " << total << endl;
     
-    
-    cout << "Your score is " << total << endl;
+    if ((total >= 28))
+        cout << "You application for BSc in computer Science was successfull.\n";
+    else
+        cout << "You application for BSc in computer Science was NOT successfull.\n";
 
     
     return 0;
